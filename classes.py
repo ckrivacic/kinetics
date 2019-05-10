39aba814348927ac3e9c97ef63e8aee54c5ed901
ckrivacic <ckrivacic@guybrush.ucsf.edu>
2019-05-10 16:25:05 -0700

    starting classes file

---
+++ classes.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import scipy
+from scipy import stats
+import numpy as np
+import pickle as pkl
+import scipy.optimize as opt
+import os, bio96
+
+class line(object):
+
+    def __init__(self):
+        
