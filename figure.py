db39dd6a0af46b91edce55a036d2144fab01e0b3
ckrivacic <krivacic@berkeley.edu>
2019-08-15 09:17:53 -0700

    figure updates

--- figure.py
+++ figure.py
@@ -7,7 +7,9 @@ Usage:
 """
 import numpy as np
 import pandas as pd
+import scipy.optimize as opt
 from klab import docopt
+from itertools import cycle
 import matplotlib.pyplot as plt
 #from kinetics import load_dataframe
 
@@ -57,11 +59,27 @@ data = import_data()
 # Get rid of excluded points
 data = data[data['clicked_kinetics']==False]
 # Create data
-N = 500
-x = data['conc_uM']
-y = data['slope']
-colors = (0,0,0)
-area = np.pi*3
+parsed_data = []
+fits = []
+for name, group in data.groupby(['date','enzyme']):
+    x = group['conc_uM']
+    y = group['slope']
+    parsed_data.append((x,y))
+
+    # Generate parameters
+    optimizedParameters, pcov = opt.curve_fit(func, x, y)
+    xModel = np.linspace(min(x),max(x),num=500)
+    yModel = func(xModel, *optimizedParameters)
+    fits.append((xModel, yModel))
+
+# Axes limits
+ymax = max(data['slope']) + 0.1 * max(data['slope'])
+ymin = min(data['slope']) - 0.1 * max(data['slope'])
+
+# Colors
+point_colors = cycle(['#C5523C', '#4BA0C1'])
+line_colors = cycle(['#F06449', '#5BC3EB'])
+area = 4*np.pi*3
 
 def plot_stuff(graphWidth, graphHeight):
     # Plot
@@ -70,10 +88,12 @@ def plot_stuff(graphWidth, graphHeight):
     axes = f.add_subplot(111)
     
     # raw data as scatter plot
-    axes.scatter(x, y, s=area, c=colors, alpha=0.5)
-    plt.title('Scatter plot pythonspot.com')
-    plt.xlabel('x')
-    plt.ylabel('y')
+    for group in parsed_data:
+        axes.scatter(group[0], group[1], s=area, c=next(point_colors), alpha=0.5)
+    for fit in fits:
+        axes.plot(fit[0],fit[1], c=next(line_colors))
+
+    axes.set_ylim([ymin, ymax])
     plt.show()
 
 plot_stuff(800,600)
