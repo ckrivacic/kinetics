26a2854b1d74a5496b306e1cc2f9384ab06a7d32
Cody <krivacic@berkeley.edu>
2019-08-16 12:29:10 -0700

    figure prettier

--- figure.py
+++ figure.py
@@ -11,40 +11,14 @@ import scipy.optimize as opt
 from klab import docopt
 from itertools import cycle
 import matplotlib.pyplot as plt
-#from kinetics import load_dataframe
+import matplotlib.ticker as mtick
+import matplotlib.pyplot as pyplot
 
 
 def func(S, Km, Vmax):
     V0 = (Vmax * S) / (Km + S)
     return V0
 
-'''
-def setup_data():
-    # Probably don't need this function
-    args = docopt.docopt(__doc__)
-    if os.path.isfile(args['<bio96_metadata>']):
-        data = bio96.load(args['<bio96_metadata>'],load_dataframe,{'well':'variable'})[0]
-    elif os.path.isdir(args['<bio96_metadata>']):
-        dataframes = []
-        for f in os.listdir(args['<bio96_metadata>']):
-            if f.endswith('.data'):
-                subdata = bio96.load(os.path.join(args['<bio96_metadata>'],f),load_dataframe,{'well':'variable'})[0]
-                #print(subdata)
-                dataframes.append(subdata)
-        data = pd.concat(dataframes,ignore_index=True)
-
-    data = data.dropna()
-
-    data['product'] = data['units'] * data['value']/data['conversion_factor']
-    if 'enzyme_conc' in data:
-        data['persecond'] = data['product'] / data['enzyme_conc']
-    else:
-        data['persecond'] = data['product']
-    data['clicked_linear'] = False
-    data['clicked_kinetics'] = False
-    data['slope'] = 1
-    return data
-'''
 def import_data():
     args = docopt.docopt(__doc__)
     dataframes = []
@@ -73,8 +47,10 @@ for name, group in data.groupby(['date','enzyme']):
     fits.append((xModel, yModel))
 
 # Axes limits
-ymax = max(data['slope']) + 0.1 * max(data['slope'])
-ymin = min(data['slope']) - 0.1 * max(data['slope'])
+ymax = max(data['slope']) + 0.05 * max(data['slope'])
+ymin = min(data['slope']) - 0.05 * max(data['slope'])
+xmax = max(data['conc_uM']) + 0.05 * max(data['conc_uM'])
+xmin = min(data['conc_uM']) - 0.05 * max(data['conc_uM'])
 
 # Colors
 point_colors = cycle(['#C5523C', '#4BA0C1'])
@@ -82,6 +58,10 @@ line_colors = cycle(['#F06449', '#5BC3EB'])
 area = 4*np.pi*3
 
 def plot_stuff(graphWidth, graphHeight):
+    font = {'family' : 'normal',
+            'size' : 12}
+    plt.rc('font', **font)
+
     # Plot
     f = plt.figure(figsize=(graphWidth/100.0,\
         graphHeight/100.0),dpi=100)
@@ -90,10 +70,25 @@ def plot_stuff(graphWidth, graphHeight):
     # raw data as scatter plot
     for group in parsed_data:
         axes.scatter(group[0], group[1], s=area, c=next(point_colors), alpha=0.5)
+    # plot the fits
     for fit in fits:
         axes.plot(fit[0],fit[1], c=next(line_colors))
 
+    pyplot.hlines(0,xmin,xmax,linestyles='dashed',label='')
+
+    # Label parameters
     axes.set_ylim([ymin, ymax])
+    axes.set_xlim([xmin, xmax])
+    axes.tick_params(width=2,length=8)
+    axes.tick_params(axis='y',which='both',labelleft=True,labelright=False,right=True)
+    axes.ticklabel_format(axis='y',style='sci',scilimits=(0,0))
+    axes.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.1e'))
+    axes.xaxis.set_tick_params(direction='in')
+    axes.yaxis.set_tick_params(direction='in')
+
+    plt.xlabel('[5(10) estrene-dione], μM')
+    plt.ylabel('V$_{0}$, s$^{-1}$')
+
     plt.show()
 
 plot_stuff(800,600)
